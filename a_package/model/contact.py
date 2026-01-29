7d5ead3a56eee692e40ec87c90e6a856d75292f3
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-01-29 23:16:59 +0100

    ENH: add RigidContact back to lib

---
+++ a_package/model/contact.py
@@ -0,0 +1,18 @@
+import numpy as np
+import numpy.fft as fft
+
+from a_package.domain import Grid, FirstOrderElement, Quadrature, centroid_quadrature, adapt_shape
+
+
+class RigidContact:
+    """Computes the gap field between two rigid surfaces at a given separation."""
+
+    def __init__(self, upper: np.ndarray, lower: np.ndarray):
+        self.upper = adapt_shape(upper)
+        self.lower = adapt_shape(lower)
+
+    def set_mean_separation(self, value: float):
+        self.separation = value
+
+    def get_gap(self):
+        return np.clip(self.separation + self.upper - self.lower, 0, None)
