248dc0965d76aaf59df0c8838faeb9ddb4245884
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-01-30 15:43:17 +0100

    MAINT: update simulation/io due to change of domain/io

--- a_package/simulation/io.py
+++ a_package/simulation/io.py
@@ -19,74 +19,86 @@ class SimulationIO:
         self._io = NpyIO(store_dir)
 
     def save_constant(self, fields: dict[str, Field]={}, single_values: dict[str, float]={}):
+        # For field, each step has its own file
         for [name, field] in fields.items():
-            self._io.save_field(self.grid, name, field)
+            self._io.save_distributed(self.grid, name, field)
 
+        # For single values, all steps shares one file
         for [name, value] in single_values.items():
-            self._io.save_value_array(name, np.array([value]))
+            self._io.save_singular(name, np.array([value]))
 
     def load_constant(self, field_names: list[str]=[], single_value_names: list[str]=[]):
         result = {}
 
         # For field, each step has its own file
         for name in field_names:
-            result[name] = self._io.load_field(self.grid, name)
+            result[name] = self._io.load_distributed(self.grid, name)
 
         # For single values, all steps shares one file
         for name in single_value_names:
-            [result[name]] = self._io.load_value_array(name)
+            [result[name]] = self._io.load_replicated(name)
 
         return result
 
     def save_step(self, index: int, fields: dict[str, Field]={}, single_values: dict[str, float]={}):
         # For field, each step has its own file
         for [name, field] in fields.items():
-            self._io.save_field(self.grid, _format_filename(name, index), field)
+            self._io.save_distributed(self.grid, _format_filename(name, index), field)
 
         # For single values, all steps share one file
         for [name, value] in single_values.items():
-            array = self._io.load_value_array(name)
             try:
-                array[index] = value
-            except IndexError:
-                if index == array.size:
-                    array = np.append(array, value)
-                else:
-                    raise ValueError()
-            self._io.save_value_array(name, array)
+                array = self._io.load_singular(name)
+            except FileNotFoundError:
+                # First step, create empty
+                array = np.array([])
+
+            # Idling processes gets None
+            if array is not None:
+                try:
+                    array[index] = value
+                except IndexError:
+                    if index == array.size:
+                        array = np.append(array, value)
+                    else:
+                        raise ValueError()
+            self._io.save_singular(name, array)
 
     def load_step(self, index: int, field_names: list[str]=[], single_value_names: list[str]=[]):
         result = {}
 
         # For field, each step has its own file
         for name in field_names:
-            result[name] = self._io.load_field(self.grid, _format_filename(name, index))
+            result[name] = self._io.load_distributed(self.grid, _format_filename(name, index))
 
         # For single values, all steps shares one file
         for name in single_value_names:
-            result[name] = self._io.load_value_array(name)[index]
+            result[name] = self._io.load_replicated(name)[index]
 
         return result
 
     def save_trajectory(self, fields: dict[str, list[Field]]={}, single_values: dict[str, np.ndarray]={}):
-        result = {}
         # For field, every step is saved in one file.
         for [name, traj] in fields.items():
             array = _FieldArray(self.grid, self._io, name)
             for index in range(len(traj)):
                 array[index] = traj[index]
+
         # For single values, a trajectory is saved as one file
         for [name, traj] in single_values.items():
-            result[name] = self._io.save_value_array(name, traj)
+            self._io.save_singular(name, traj)
 
     def load_trajectory(self, field_names: list[str]=[], single_value_names: list[str]=[]):
         result = {}
+
         # For field, every step is saved in one file.
         for name in field_names:
             result[name] = _FieldArray(self.grid, self._io, name)
+
         # For single values, a trajectory is saved as one file
         for name in single_value_names:
-            result[name] = self._io.load_value_array(name)
+            result[name] = self._io.load_singular(name)
+
         return result
 
 
@@ -108,10 +120,10 @@ class _FieldArray:
         self._name = name
 
     def __getitem__(self, index: int):
-        return self._io.load_field(self.grid, _format_filename(self._name, index))
+        return self._io.load_distributed(self.grid, _format_filename(self._name, index))
 
     def __setitem__(self, index: int, value):
-        self._io.save_field(self.grid, _format_filename(self._name, index), value)
+        self._io.save_distributed(self.grid, _format_filename(self._name, index), value)
 
 
 def _format_filename(name: str, index: int):
