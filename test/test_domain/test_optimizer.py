8df68b1c7ee611b9670f794dc4eaf3865aff1c9d
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-03-19 14:37:42 +0100

    TST: add test cases for optimizer with different constraints

---
+++ test/test_domain/test_optimizer.py
@@ -0,0 +1,121 @@
+import types
+
+import numpy as np
+
+from a_package.domain.optimizer import Optimizer
+
+
+def test_unconstrained_numopt():
+
+    saved_x = np.zeros(0)
+
+    def get_x():
+        return saved_x
+
+    def set_x(x):
+        nonlocal saved_x
+        saved_x = x
+
+    def get_f():
+        return np.sum(saved_x**2)
+
+    def get_f_Dx():
+        return 2 * saved_x
+
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx)
+
+    optimizer = Optimizer(max_loop=1)
+    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]))
+
+    assert np.all(np.isclose(result['primal'], 0.))
+
+
+def test_eq_constrained_numopt():
+
+    saved_x = np.zeros(0)
+
+    def get_x():
+        return saved_x
+
+    def set_x(x):
+        nonlocal saved_x
+        saved_x = x
+
+    def get_f():
+        return np.sum(saved_x**2)
+
+    def get_f_Dx():
+        return 2 * saved_x
+
+    def get_g():
+        x1, x2 = saved_x
+        return (x1 - 2)**2 + x2**2 - 1
+
+    def get_g_Dx():
+        x1, x2 = saved_x
+        return np.array([2 * (x1 - 2), 2 * x2])
+
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, get_g=get_g, get_g_Dx=get_g_Dx)
+
+    optimizer = Optimizer(max_loop=5)
+    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]), lam0=0., alpha0=1e1)
+
+    assert np.all(np.isclose(result['primal'], [1., 0.]))
+
+
+def test_bound_constrained_numopt():
+
+    saved_x = np.zeros(0)
+
+    def get_x():
+        return saved_x
+
+    def set_x(x):
+        nonlocal saved_x
+        saved_x = x
+
+    def get_f():
+        return np.sum(saved_x**2)
+
+    def get_f_Dx():
+        return 2 * saved_x
+
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, x_lb=1., x_ub=2.)
+
+    optimizer = Optimizer(max_loop=1)
+    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]))
+
+    assert np.all(np.isclose(result['primal'], 1.))
+
+
+def test_eq_and_bound_constrained_numopt():
+
+    saved_x = np.zeros(0)
+
+    def get_x():
+        return saved_x
+
+    def set_x(x):
+        nonlocal saved_x
+        saved_x = x
+
+    def get_f():
+        return np.sum(saved_x**2)
+
+    def get_f_Dx():
+        return 2 * saved_x
+
+    def get_g():
+        x1, x2 = saved_x
+        return (x1 - 2)**2 + x2**2 - 1
+
+    def get_g_Dx():
+        x1, x2 = saved_x
+        return np.array([2 * (x1 - 2), 2 * x2])
+
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, get_g=get_g, get_g_Dx=get_g_Dx, x_lb=np.array([2., 0.]), x_ub=10.)
+
+    optimizer = Optimizer(max_loop=50)
+    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 0.]), lam0=0., alpha0=1e1, beta0=1e-3)
+
+    assert np.all(np.isclose(result['primal'], [2., 1.]))
