6f7f6a76880d82e54cb8b1353368fe758a0b3c7d
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-03-19 21:43:53 +0100

    TST: More deviated initial values for optimizer tests

--- test/test_domain/test_optimizer.py
+++ test/test_domain/test_optimizer.py
@@ -25,9 +25,10 @@ def test_unconstrained_numopt():
     num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx)
 
     optimizer = Optimizer(max_loop=1)
-    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]))
+    result = optimizer.solve_minimisation(num_opt, x0=[10, 10])
 
     assert np.all(np.isclose(result['primal'], 0.))
+    assert result['is_converged']
 
 
 def test_eq_constrained_numopt():
@@ -57,10 +58,11 @@ def test_eq_constrained_numopt():
 
     num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, get_g=get_g, get_g_Dx=get_g_Dx)
 
-    optimizer = Optimizer(max_loop=5)
-    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]), lam0=0., alpha0=1e1)
+    optimizer = Optimizer(max_loop=10)
+    result = optimizer.solve_minimisation(num_opt, x0=[10., 10.], lam0=0., alpha0=1e1)
 
     assert np.all(np.isclose(result['primal'], [1., 0.]))
+    assert result['is_converged']
 
 
 def test_bound_constrained_numopt():
@@ -80,12 +82,13 @@ def test_bound_constrained_numopt():
     def get_f_Dx():
         return 2 * saved_x
 
-    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, x_lb=1., x_ub=2.)
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, x_lb=np.array([2., -1]), x_ub=10.)
 
-    optimizer = Optimizer(max_loop=1)
-    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 1.]))
+    optimizer = Optimizer(max_loop=5)
+    result = optimizer.solve_minimisation(num_opt, x0=[10., 10.], beta0=1e-2)
 
-    assert np.all(np.isclose(result['primal'], 1.))
+    assert np.all(np.isclose(result['primal'], [2., 0.]))
+    assert result['is_converged']
 
 
 def test_eq_and_bound_constrained_numopt():
@@ -113,9 +116,10 @@ def test_eq_and_bound_constrained_numopt():
         x1, x2 = saved_x
         return np.array([2 * (x1 - 2), 2 * x2])
 
-    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, get_g=get_g, get_g_Dx=get_g_Dx, x_lb=np.array([2., 0.]), x_ub=10.)
+    num_opt = types.SimpleNamespace(get_x=get_x, set_x=set_x, get_f=get_f, get_f_Dx=get_f_Dx, get_g=get_g, get_g_Dx=get_g_Dx, x_lb=np.array([2., -1.]), x_ub=10.)
 
-    optimizer = Optimizer(max_loop=50)
-    result = optimizer.solve_minimisation(num_opt, x0=np.array([0., 0.]), lam0=0., alpha0=1e1, beta0=1e-3)
+    optimizer = Optimizer(max_loop=10)
+    result = optimizer.solve_minimisation(num_opt, x0=np.array([10., 10.]), lam0=0., alpha0=1e1, beta0=1e-2)
 
     assert np.all(np.isclose(result['primal'], [2., 1.]))
+    assert result['is_converged']
