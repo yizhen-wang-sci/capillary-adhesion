cd299224a03393d6e83f31295ed01331adb43267
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-05-19 14:36:46 +0200

    TEST: fix stitching field with proper NumPy stack; extend decomposition test for both x and y directions.

--- test/test_domain/test_grid.py
+++ test/test_domain/test_grid.py
@@ -15,8 +15,8 @@ def ref_field():
     return np.arange(100).reshape((10, 10))
 
 
-def test_real_field_decomposition(ref_field, comm_world):
-    shape = (10, 10)
+def test_real_field_x_decomposition(ref_field, comm_world):
+    shape = ref_field.shape
     grid = Grid(shape)
 
     # Decompose along the non-contiguous axis (for NumPy it is axis 0)
@@ -25,6 +25,21 @@ def test_real_field_decomposition(ref_field, comm_world):
     field = collection.real_field("test_field", 1)
     field.s[0, 0, ...] = ref_field[tuple(decomposition.icoords)]
 
-    # NumPy reshape stitches the non-contiguous axis properly
-    collected = np.reshape(comm_world.allgather(field.s), shape)
+    # NumPy vstack stitches the non-contiguous axis properly
+    collected = np.vstack(comm_world.allgather(field.s[0, 0, ...]))
+    assert np.allclose(collected, ref_field)
+
+
+def test_real_field_y_decomposition(ref_field, comm_world):
+    shape = ref_field.shape
+    grid = Grid(shape)
+
+    # Decompose along the contiguous axis (for NumPy it is axis -1)
+    decomposition = grid.decompose((1, comm_world.Get_size()), nb_ghost_layers=(1, 1), communicator=comm_world)
+    collection = decomposition.collection
+    field = collection.real_field("test_field", 1)
+    field.s[0, 0, ...] = ref_field[tuple(decomposition.icoords)]
+
+    # NumPy hstack stitches the contiguous axis properly
+    collected = np.hstack(comm_world.allgather(field.s[0, 0, ...]))
     assert np.allclose(collected, ref_field)
