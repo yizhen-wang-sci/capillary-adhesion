7b0115131194532a832d3319be1edf7d8bc07273
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-05-21 10:57:12 +0200

    MAINT: add optional `communicator` parameter to `NpyIO` for improved interoperation with NuMPI

--- a_package/domain/io.py
+++ a_package/domain/io.py
@@ -9,15 +9,12 @@ import NuMPI.IO
 from NuMPI import MPI
 
 
-_comm = MPI.COMM_WORLD
-
-
 class NpyIO:
     """
     NumPy-based parallel-aware data persistence.
     """
 
-    def __init__(self, root_path, decomposition=None):
+    def __init__(self, root_path, decomposition=None, communicator=None):
         self.root_path = pathlib.Path(root_path)
 
         if decomposition is None:
@@ -30,19 +27,26 @@ class NpyIO:
             self._nb_subdomain_grid_pts = tuple(decomposition.nb_subdomain_grid_pts)
             self._nb_domain_grid_pts = tuple(decomposition.nb_domain_grid_pts)
 
+        if communicator is None:
+            self._comm = MPI.COMM_SELF
+        else:
+            self._comm = communicator
+
     def _to_full_path(self, name: str):
         return self.root_path / f"{name}.npy"
 
     def load_distributed(self, name: str):
         return NuMPI.IO.load_npy(self._to_full_path(name),
                                  self._subdomain_locations,
-                                 self._nb_subdomain_grid_pts)
+                                 self._nb_subdomain_grid_pts,
+                                 comm=self._comm)
 
     def save_distributed(self, name: str, data):
         NuMPI.IO.save_npy(self._to_full_path(name),
                           np.ascontiguousarray(data),
                           self._subdomain_locations,
-                          self._nb_domain_grid_pts)
+                          self._nb_domain_grid_pts,
+                          comm=self._comm)
 
     def load_singular(self, name: str):
         if _comm.rank == 0:
