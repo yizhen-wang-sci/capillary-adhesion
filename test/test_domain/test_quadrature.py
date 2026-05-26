fe29e15988741fe6087dbb400d264d531c6df6cf
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2026-05-26 14:49:26 +0200

    TEST: update quadrature tests to use communicators explicitly

--- test/test_domain/test_quadrature.py
+++ test/test_domain/test_quadrature.py
@@ -1,26 +1,21 @@
 import pytest
 
 import numpy as np
-from NuMPI import MPI
 
-from a_package.domain.quadrature import Quadrature, centroid_quadrature, three_pt_quadrature
+from a_package.domain.quadrature import NodalQuadrature, CentroidQuadrature, ThreePtQuadrature
 
 from test.test_domain.utils import generate_global_random_field
 
 
-nodal_quadrature = Quadrature([[0., 0.]], [1.])
-
-
-@pytest.fixture(params=[nodal_quadrature, centroid_quadrature, three_pt_quadrature])
-def mock_quadrature(request):
-    return request.param
+@pytest.fixture(params=[NodalQuadrature, CentroidQuadrature, ThreePtQuadrature])
+def mock_quadrature(request, comm_world):
+    return request.param(comm_world)
 
 
 def test_integral(decomposed_grid, mock_quadrature, comm_world):
     field = generate_global_random_field(decomposed_grid.nb_domain_grid_pts, comm_world)
     local_field = np.expand_dims(field[*decomposed_grid.decomposition.icoords], axis=(0, 1))
     value = mock_quadrature.integrate(local_field)
-    value = comm_world.allreduce(value, op=MPI.SUM)
     expected_value = np.sum(field)
 
     assert np.allclose(value, expected_value, atol=1e-12)
@@ -31,9 +26,10 @@ def test_integral_perturbed(decomposed_grid, mock_quadrature, comm_world):
     perturb = 1e-3
     for idx in np.ndindex(decomposed_grid.nb_domain_grid_pts):
         field[idx] += perturb
+
         local_field = np.expand_dims(field[*decomposed_grid.decomposition.icoords], axis=(0, 1))
         value = mock_quadrature.integrate(local_field)
-        value = comm_world.allreduce(value, op=MPI.SUM)
         expected_value = np.sum(field)
         assert np.allclose(value, expected_value, atol=1e-12)
+
         field[idx] -= perturb
